4fca0d8a8930c9c02da27f6aba2616c15d898920
kirill6570 <kirill6570@github.com>
2026-03-12 23:16:28 +0700

    по красоте

--- main.cpp
+++ main.cpp
@@ -5,10 +5,17 @@
 
 using namespace std;
 
+bool isVowel(wchar_t ch) {
+    ch = towlower(ch);
+    wstring vowels = L"aeiouаеёиоуыэюя";
+    return vowels.find(ch) != wstring::npos;
+}
+
 int main() {
-    locale::global(locale("ru_RU.utf8"));
-    wcin.imbue(locale());
-    wcout.imbue(locale());
+    locale loc("ru_RU.utf8");
+    locale::global(loc);
+    wcin.imbue(loc);
+    wcout.imbue(loc);
 
     wstring input;
     wchar_t choice;
@@ -16,17 +23,25 @@ int main() {
     wcout << L"Введите строку: ";
     getline(wcin, input);
 
+    int vowelCount = 0;
+    for (wchar_t ch : input) {
+        if (isVowel(ch)) vowelCount++;
+    }
+    wcout << L"Количество гласных: " << vowelCount << endl;
+
     wcout << L"Выберите регистр (L - нижний, U - верхний): ";
     wcin >> choice;
 
     if (choice == L'L' || choice == L'l') {
-        for (wchar_t &ch : input) ch = towlower(ch);
+        for (wchar_t &ch : input)
+            ch = towlower(ch);
         wcout << L"Строка в нижнем регистре: " << input << endl;
-    } 
+    }
     else if (choice == L'U' || choice == L'u') {
-        for (wchar_t &ch : input) ch = towupper(ch);
+        for (wchar_t &ch : input)
+            ch = towupper(ch);
         wcout << L"Строка в верхнем регистре: " << input << endl;
-    } 
+    }
     else {
         wcout << L"Некорректный выбор регистра!" << endl;
         return 1;
