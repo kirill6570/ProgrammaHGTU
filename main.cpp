e71c96c2b67a166577615fd68d4375d9e47044c4
kirill6570 <kirill6570@github.com>
2026-03-13 12:28:13 +0700

    красота

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <locale>
+#include <cwctype>
+#include <string>
+
+using namespace std;
+
+bool isVowel(wchar_t ch) {
+    ch = towlower(ch);
+    wstring vowels = L"aeiouаеёиоуыэюя";
+    return vowels.find(ch) != wstring::npos;
+}
+
+int main() {    
+    locale loc("ru_RU.utf8");
+    locale::global(loc);
+    wcin.imbue(loc);
+    wcout.imbue(loc);
+
+    wstring input;
+    wchar_t choice;
+
+    wcout << L"Введите строку: ";
+    getline(wcin, input);
+
+    int vowelCount = 0;
+    for (wchar_t ch : input) {
+        if (isVowel(ch)) vowelCount++;
+    }
+    wcout << L"Количество гласных: " << vowelCount << endl;
+
+    wcout << L"Выберите регистр (L - нижний, U - верхний): ";
+    wcin >> choice;
+
+    if (choice == L'L' || choice == L'l') {
+        for (wchar_t &ch : input)
+            ch = towlower(ch);
+        wcout << L"Строка в нижнем регистре: " << input << endl;
+    }
+    else if (choice == L'U' || choice == L'u') {
+        for (wchar_t &ch : input)
+            ch = towupper(ch);
+        wcout << L"Строка в верхнем регистре: " << input << endl;
+    }
+    else {
+        wcout << L"Некорректный выбор регистра!" << endl;
+        return 1;
+    }
+
+    return 0;
+}	
